d5205549bb2230a1d28a580a44007b860dce1506
GiorgiBasaria <86610362+GiorgiBasaria@users.noreply.github.com>
2023-02-28 16:51:01 +0100

    Add files via upload

---
+++ ejer1JavaBasico/src/com/ejemplo/Second.java
@@ -0,0 +1,15 @@
+package com.ejemplo;
+
+public class Second {
+    public static void main(String[] args) {
+        int num = 5;
+        double numD = 5d;
+        float numF = 1.2f;
+        boolean verdad = false;
+        String nombre = "Gio";
+
+        System.out.println(num + " " + numD + " " + numF + " " + verdad + " " + nombre);
+
+    }
+
+}
